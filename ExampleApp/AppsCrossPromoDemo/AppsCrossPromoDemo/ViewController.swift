e07c26cafa0e18e3e791ec2164ed8c4960ddde3a
cuipanjun <andy380743909@gmail.com>
2025-03-26 22:26:32 +0800

    complete demo

--- ExampleApp/AppsCrossPromoDemo/AppsCrossPromoDemo/ViewController.swift
+++ ExampleApp/AppsCrossPromoDemo/AppsCrossPromoDemo/ViewController.swift
@@ -19,11 +19,24 @@ class ViewController: UIViewController {
     @IBAction func go2AppList(_ sender: Any) {
         
         // Create apps list
-        let app1 = AppInfo(AppID: 2000000, name: "Game One", subtitle: "make and run love2d games", iconName: "game_one_icon", appStoreLink: "https://apps.apple.com/app/id123456")
-        let app2 = AppInfo(AppID: 2000000, name: "Game Two Game Two Game Two Game Two Game Two Game Two Game Two", subtitle: "write and execute lua scripts write and execute lua scripts write and execute lua script swrite and execute lua scripts", iconName: "game_two_icon", appStoreLink: "https://apps.apple.com/app/id654321")
+        let app1 = AppInfo(AppID: 638219114,
+                           name: "LuaLu REPL",
+                           subtitle: "Learn Lua Coding. It's not luau!",
+                           iconName: "LuaLuApp",
+                           appStoreLink: "https://apps.apple.com/us/app/lualu-repl-learn-lua-coding/id638219114")
+        let app2 = AppInfo(AppID: 6476174098,
+                           name: "Love2D Game Maker",
+                           subtitle: "develop and run Love2D games",
+                           iconName: "Love2DGM",
+                           appStoreLink: "https://apps.apple.com/us/app/love2d-game-maker/id6476174098")
+        let app3 = AppInfo(AppID: 6498889447,
+                           name: "Solar2D Studio - CoronaSDK",
+                           subtitle: "Make Apps/Games on your phone using Lua!",
+                           iconName: "Solar2DStudio",
+                           appStoreLink: "https://apps.apple.com/us/app/solar2d-studio-coronasdk/id6498889447")
 
         // Add to Array
-        let appsArray = [app1, app2]
+        let appsArray = [app1, app2, app3]
 
         // Create ViewController
         let appsVC = AppsListViewController(appsArray: appsArray as NSArray)
